e92bbbd0ffd2e07034af0487aec46990c3b213fb
davbt <davidbatonda7@gmail.com>
2026-03-28 23:51:49 +1100

    Use correct env variables for Dockerfile + Tidying
    - ROS2 Launch has been faulty with the automatic launches and only
      manual launches would work well. Fix was to setup environment
      correctly (e.g. RMW DDS, CMAKE, LD_LIBRARY env vars)
    - `Reset` gRPC method is essentially identical to `SetInitialPose`,
      hence remove it
    - Some tidying of README as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 MAP_DIR="${AUTOWARE_MAP_DIR:-$HOME/autoware_map}"
 MAP_ZIP="${MAP_DIR}/sample-map-planning.zip"
-
 LAUNCH_ROS2="${LAUNCH_ROS2:-true}"
+ENABLE_VNC="${ENABLE_VNC:-false}"
 
 if [ ! -d "$MAP_DIR/sample-map-planning" ]; then
   mkdir -p "$MAP_DIR"
@@ -13,23 +13,33 @@ if [ ! -d "$MAP_DIR/sample-map-planning" ]; then
 fi
 
 if [ "$ENABLE_VNC" = "true" ]; then
-  export DISPLAY=${DISPLAY:-:1}
+  export DISPLAY="${DISPLAY:-:1}"
   /start_vnc.sh &
 fi
 
 source /opt/autoware/setup.bash
+
+if [ $# -gt 0 ]; then
+  exec "$@"
+fi
+
+cleanup() {
+  kill "$GRPC_PID" "$SIM_PID" 2>/dev/null
+  wait
+}
+trap cleanup INT TERM
 
 python3 /app/server_node.py &
 GRPC_PID=$!
 
 if [ "$LAUNCH_ROS2" = "true" ]; then
   ros2 launch autoware_launch planning_simulator.launch.xml \
-    map_path:=$MAP_DIR/sample-map-planning \
+    map_path:="$MAP_DIR"/sample-map-planning \
     vehicle_model:=sample_vehicle \
     sensor_model:=sample_sensor_kit \
-    rviz:=$ENABLE_VNC &
-  PLANNING_PID=$!
-  wait -n "$PLANNING_PID" "$GRPC_PID"
+    rviz:="$ENABLE_VNC" &
+  SIM_PID=$!
+  wait -n "$SIM_PID" "$GRPC_PID"
 else
   wait "$GRPC_PID"
 fi
